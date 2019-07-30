c1a51e8b1b5b5fbaf994e48c8a1d4412d5549abb
joyjxu <joyjxu@tencent.com>
2019-07-30 21:43:47 +0800

    add test cases for afm and dcn

--- angelml/src/test/scala/org/apache/spark/angelml/classification/AngelClassificationSuite.scala
+++ angelml/src/test/scala/org/apache/spark/angelml/classification/AngelClassificationSuite.scala
@@ -281,4 +281,38 @@ class AngelClassificationSuite extends SparkFunSuite {
 
 //    driverCtx.stopAngelAndPSAgent()
   }
+
+  test("afm_train") {
+    val trainData = libsvm.load("data/angel/census/census_148d_train.libsvm")
+
+    val classifier = new AngelClassifier()
+      .setModelJsonFile("./angelml/src/test/jsons/afm.json")
+      .setNumClass(2)
+      .setNumBatch(10)
+      .setMaxIter(2)
+      .setLearningRate(0.1)
+      .setNumField(13)
+
+    val model = classifier.fit(trainData)
+
+    model.write.overwrite().save("trained_models/afm")
+
+  }
+
+  test("dcn_train") {
+    val trainData = libsvm.load("data/angel/census/census_148d_train.libsvm")
+
+    val classifier = new AngelClassifier()
+      .setModelJsonFile("./angelml/src/test/jsons/dcn.json")
+      .setNumClass(2)
+      .setNumBatch(10)
+      .setMaxIter(2)
+      .setLearningRate(0.1)
+      .setNumField(13)
+
+    val model = classifier.fit(trainData)
+
+    model.write.overwrite().save("trained_models/pnn")
+
+  }
 }
